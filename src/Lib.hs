8f37afd6220addd3439e9f5c3927072ebd8db4bc
David J. Gordon <djyale@gmail.com>
2015-09-19 19:45:50 -0400

    Correctly divides functions into changed and unchanged types. Next, we need to remove duplicates

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,56 +3,130 @@ module Lib
     ( 
     ) where
 
-import           Control.Arrow ((***))
-import qualified Data.Set as S
-import qualified Hoogle as H
-import qualified Data.Map as M
 import           Control.Applicative ((<$>), (<*>), pure)
+import           Control.Arrow ((***), (&&&))
+import qualified Data.Map as M
+import qualified Data.Set as S
+import           Debug.Trace
+import qualified Hoogle as H
 
-type FunctionSignature = (FunctionName, Type)
+type FunctionSignature = (ModuleName, FunctionName, Type)
 type FunctionName = String
 type Type = String
+type ModuleName = String
 
-data Changelog = Changelog {added 
-                           ,deleted 
-                           ,changedType ::  S.Set FunctionSignature}
-                            deriving Show
+{-data Changelog = Changelog {added :: S.Set FunctionSignature
+                                  ,deleted :: S.Set FunctionSignature
+                                  ,changedType ::  S.Set FunctionSignature}
+                                   deriving Show-}
+       
+data Changelog a = Changelog {added :: [FunctionSignature]
+                             ,deleted :: [FunctionSignature]
+                             ,changedType :: [(FunctionSignature,
+                                               FunctionSignature)]
+                             ,unchanged :: [FunctionSignature]} deriving Show
 
-compareModules
+
+
+{-compareModules
   :: FilePath
      -> FilePath
-     -> IO Changelog
+     -> IO Changelog-}
 compareModules oldVersion newVersion = do
   [old, new] <- mapM (fmap processResults . getHoogleTags)
                                         [oldVersion, newVersion]
-  return $ buildChangelog new old
+  return $ buildChangelog' new old
 
-buildChangelog :: [FunctionSignature] -> [FunctionSignature] -> Changelog
-buildChangelog new old = Changelog (S.fromList added') (S.fromList deleted') (S.fromList changedType')
-  where
-    (added', deleted', changedType') = foldr chooseBucket ([], [], []) (old ++ new)
-                 
-    chooseBucket sig@(n, t) (as, ds, cs)
-      | n `S.member` bothSet = (as, ds, sig:cs)
-      | n `S.member` newSet  = (sig:as, ds, cs)
-      | n `S.member` oldSet  = (as, sig:ds, cs)
-    newSet = S.fromList $ map fst new
-    oldSet = S.fromList $ map fst old
-    bothSet = newSet `S.intersection` oldSet
+data FunctionStatus a = Deleted a | ChangedType {old :: a
+                                                ,new :: a} | Unchanged a | Added a
+                         deriving Show
+                         
+ 
 
-
-
-
-processResults :: [(a, H.Result)] -> [FunctionSignature]
-processResults = map (parseSignature . H.self . snd)
+--buildChangelog' :: [FunctionSignature] -> [FunctionSignature] -> Changelog
+buildChangelog' new old = Changelog added' deleted' changeType' unchanged'
   where 
-    parseSignature = both H.showTagText . split . unwrap
+    (added', deleted', changeType', unchanged') = foldr step ([], [], [], []) 
+                                                             (oldInNew ++ newInOld)
 
-    unwrap (H.Tags x) = x
-    unwrap x = error $ "Unexpected Tags format: " ++ show x
+    step x (a, d, c, u) = case x of
+                            Added v -> (v:a, d, c, u)
+                            Deleted v -> (a, v:d, c, u)
+                            ChangedType o n -> (a, d, (o, n):c, u)
+                            Unchanged v -> (a, d, c, v:u)
+
+    oldInNew = do
+      sig@(n, m, t) <- old
+      return $ case (n, m) `M.lookup` newM of
+                 Nothing -> Deleted sig
+                 Just typeName -> if typeName == t 
+                                    then Unchanged sig 
+                                    else ChangedType {old = sig,new = (n,m, typeName)}
+
+    newInOld = do
+      sig@(n, m, t) <- new
+      return $ case (n, m) `M.lookup` oldM of
+               Nothing -> Added sig
+               Just typeName -> if typeName == t
+                                  then Unchanged  sig
+                                  else ChangedType {old = (n,
+                                                           m,
+                                                           typeName),
+                                                    new = sig}
+
+    makeMap = M.fromList . map (\(a,b,c) -> ((a, b), c))
+    [newM, oldM] = map makeMap [new, old]
+
+--buildChangelog :: [FunctionSignature] -> [FunctionSignature] -> Changelog
+--buildChangelog new old = Changelog (S.fromList added') (S.fromList deleted') (S.fromList changedType')
+  --where
+    {-(added', deleted', changedType') = foldr chooseBucket ([], [], []) (old ++ new)
+                 
+    chooseBucket sig@(m, n, t) (as, ds, cs)
+      | (m, n) `S.member` bothSet = case version sig of
+                                       NewVersion -> (as, ds, cs)
+                                       OldVersion -> (as, ds, sig:cs)
+                                       BothVersions -> undefined
+
+      | (m, n) `S.member` newSetNameModule  = (sig:as, ds, cs)
+      | (m, n) `S.member` oldSetNameModule  = (as, sig:ds, cs)
+    newSetNameModule = S.fromList $ map sndThree new
+    oldSetNameModule = S.fromList $ map sndThree old
+    bothSet = newSetNameModule `S.intersection` oldSetNameModule
+    newSetSig = S.fromList new
+    oldSetSig = S.fromList old
+    version x = if x `S.member` newSetSig
+                then if x `S.member` oldSetSig
+                        then BothVersions
+                        else NewVersion
+                else if x `S.member` oldSetSig
+                     then OldVersion
+                     else error $ show x ++ " not showing up in either set."
+    sndThree (a, b, c) = (a, b)-}
+
+
+
+
+
+processResults :: [(H.Score, H.Result)] ->  [(ModuleName, FunctionName, Type)]
+processResults = map (combine . (getModuleName &&& getFunction) . snd )
+  where 
+    
+    combine (a, (b, c)) = (a, b, c)
+    
+
+    
       
-    split = (head *** H.Tags) . splitAt 2 
-    both f = f *** f
+     
+both f = f *** f
+unwrap (H.Tags x) = x
+unwrap x = error $ "Unexpected Tags format: " ++ show x         
+split = (head *** H.Tags) . splitAt 2       
+parseSignature :: H.TagStr -> (FunctionName, Type)
+parseSignature = both H.showTagText . split . unwrap             
+
+getFunction =  parseSignature . H.self
+getModuleName = snd . (!! 1) . snd . head . H.locations 
 
 
 
