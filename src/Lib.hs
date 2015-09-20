0f8160cbc5dc52927e3e53a058e927312cce2955
David J. Gordon <djyale@gmail.com>
2015-09-19 20:37:28 -0400

    Stylish haskell, exported compareModules, removed type variable for Changelog data type

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,23 +1,23 @@
 -- GenChangelog Project
 module Lib
-    ( 
+    (compareModules
     ) where
 
-import           Control.Applicative ((<$>), (<*>), pure)
-import           Control.Arrow ((***), (&&&))
-import           Data.List (nub)
-import qualified Data.Map as M
-import qualified Data.Set as S
+import           Control.Applicative (pure, (<$>), (<*>))
+import           Control.Arrow       ((&&&), (***))
+import           Data.List           (nub)
+import qualified Data.Map            as M
+import qualified Data.Set            as S
 import           Debug.Trace
-import qualified Hoogle as H
+import qualified Hoogle              as H
 
 type FunctionSignature = (ModuleName, FunctionName, Type)
 type FunctionName = String
 type Type = String
 type ModuleName = String
 
-       
-data Changelog a = Changelog {added :: [FunctionSignature]
+
+data Changelog = Changelog {added :: [FunctionSignature]
                              ,deleted :: [FunctionSignature]
                              ,changedType :: [(FunctionSignature,
                                                FunctionSignature)]
@@ -33,14 +33,14 @@ compareModules oldVersion newVersion = do
 data FunctionStatus a = Deleted a | ChangedType {old :: a
                                                 ,new :: a} | Unchanged a | Added a
                          deriving Show
-                         
- 
 
-buildChangelog' new old = Changelog added' deleted' 
-                                           (nub changeType') 
+
+
+buildChangelog' new old = Changelog added' deleted'
+                                           (nub changeType')
                                            (nub unchanged')
-  where 
-    (added', deleted', changeType', unchanged') = foldr step ([], [], [], []) 
+  where
+    (added', deleted', changeType', unchanged') = foldr step ([], [], [], [])
                                                              (oldInNew ++ newInOld)
 
     step x (a, d, c, u) = case x of
@@ -53,8 +53,8 @@ buildChangelog' new old = Changelog added' deleted'
       sig@(n, m, t) <- old
       return $ case (n, m) `M.lookup` newM of
                  Nothing -> Deleted sig
-                 Just typeName -> if typeName == t 
-                                    then Unchanged sig 
+                 Just typeName -> if typeName == t
+                                    then Unchanged sig
                                     else ChangedType {old = sig,new = (n,m, typeName)}
 
     newInOld = do
@@ -75,22 +75,22 @@ buildChangelog' new old = Changelog added' deleted'
 
 processResults :: [(H.Score, H.Result)] ->  [(ModuleName, FunctionName, Type)]
 processResults = map (combine . (getModuleName &&& getFunction) . snd )
-  where 
+  where
     combine (a, (b, c)) = (a, b, c)
-    
 
-    
-      
-     
+
+
+
+
 both f = f *** f
 unwrap (H.Tags x) = x
-unwrap x = error $ "Unexpected Tags format: " ++ show x         
-split = (head *** H.Tags) . splitAt 2       
+unwrap x = error $ "Unexpected Tags format: " ++ show x
+split = (head *** H.Tags) . splitAt 2
 parseSignature :: H.TagStr -> (FunctionName, Type)
-parseSignature = both H.showTagText . split . unwrap             
+parseSignature = both H.showTagText . split . unwrap
 
 getFunction =  parseSignature . H.self
-getModuleName = snd . (!! 1) . snd . head . H.locations 
+getModuleName = snd . (!! 1) . snd . head . H.locations
 
 
 
@@ -99,9 +99,9 @@ queryString = ":: a"
 
 query :: H.Query
 query = either err id (H.parseQuery H.Haskell queryString)
-  where 
+  where
     err x = error $ ("Hoogle has changed its query language!"
-                     ++ " and has thrown error: " ++ show x) 
+                     ++ " and has thrown error: " ++ show x)
 
 getHoogleTags :: FilePath -> IO [(H.Score, H.Result)]
 getHoogleTags dbPath = H.search <$> H.loadDatabase dbPath <*> pure query
