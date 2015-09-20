f4c9645ef7a63d12e311425f3b215f2ffcf43c49
David J. Gordon <djyale@gmail.com>
2015-09-19 20:53:11 -0400

    Command line usage added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
-import Lib
+import           Lib
+import           System.Environment
 
 main :: IO ()
-main = someFunc
+main = do
+  self <- getProgName
+  args <- getArgs
+  if length args /= 2
+     then putStrLn $ ("Usage:\n" 
+                           ++ self 
+                           ++ " [old hoogle db path] [new hoogle db path]\n")
+     else let [oldVersion, newVersion] = args
+          in do
+              cl <- fmap show $  compareModules oldVersion newVersion
+              putStrLn $ show cl
