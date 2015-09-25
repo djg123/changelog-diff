8e6453f1b8abeba108d49a8a54fe7634fe4e4143
David J. Gordon <djyale@gmail.com>
2015-09-24 20:57:00 -0400

    Fleshed out format function. Discovered bug in
    in groupByModule.

--- src/FormatConsole.hs
+++ src/FormatConsole.hs
@@ -1,11 +1,27 @@
 module FormatConsole (format) where
 
 import qualified Changelog as C
+import           Data.List (intercalate)
 import qualified Data.Map as M
+
 
 format :: C.Changelog -> String
 format cl = let m = C.groupByModule cl
-            in undefined
+            in unlines $ map (\(mN, cl') ->  mN    
+                                          ++ (clPerModule cl'))
+                             (M.toAscList m)
+
+clPerModule :: C.Changelog -> String
+clPerModule cl = sep ++ (intercalate sep $ map sigToString (C.clAdded cl)
+                                                         ++ map sigToString (C.clDeleted cl)
+                                                         ++ map changedTypeSigToString (C.clChangedType cl))
+  where 
+    sigToString (_, fN, tN) = fN ++ " :: " ++ tN
+    changedTypeSigToString ((_, fN, tN),
+                            (_, _, tN')) = intercalate sep [fN ++ " :: " ++ tN'
+                                                           ,fN ++ " :: " ++ tN']
+
+    sep = "\n  "
 
 
 
