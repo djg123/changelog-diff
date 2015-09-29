fab861ffca53f490b70da1a6bfa061256011a6a8
David J. Gordon <djyale@gmail.com>
2015-09-28 21:53:28 -0400

    Arbitrary instances for Changelog.
    Now, need to ensure that we have no repeating
    (Module, Function) pairs created.

--- src/Changelog.hs
+++ src/Changelog.hs
@@ -1,12 +1,15 @@
 module Changelog (compareModules, Changelog(..), groupByModule) where
 
+import           Control.Monad (replicateM)
+import           Data.Char (isAlpha)
 import           Data.List (nub)
 import qualified Data.Map as M
 import           Data.Monoid ((<>))
 import qualified Data.Set as S
-import           Data.Tuple.Extra (both, (&&&), (***))
+import           Data.Tuple.Extra (both, (&&&), (***), fst3)
 import qualified Hoogle as H
 import           Test.QuickCheck (Arbitrary(..))
+import qualified Test.QuickCheck as T
 
 type FunctionSignature = (ModuleName, FunctionName, Type)
 type FunctionName = String
@@ -31,7 +34,70 @@ instance Monoid Changelog where
     }
 
 instance Arbitrary Changelog where
-  arbitrary = Changelog <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+  arbitrary = do
+    oldList <- makeFunctionSignatureList
+    let modules = map fst3 oldList
+    (deleted, notDeleted) <- sublistOfWithRemainder oldList -- Entries from oldList that were
+                                                            -- "removed" in later
+
+
+    (toChangeType, unchanged) <- sublistOfWithRemainder notDeleted -- Entries not deleted, but to
+                                                                   -- have
+
+             -- their types changed
+    changedType <- mapM
+                     (\(m, n, t) ->
+                        ( nonNullString) >>= \t' ->
+                          return (m, n, t'))
+                     toChangeType
+    added <- makeFunctionAndTypeList modules -- List of random functions and types only to be "added"
+
+    return
+      Changelog
+        { clAdded = added
+        , clDeleted = deleted
+        , clChangedType = toChangeType `zip` changedType
+        , clUnchanged = unchanged
+        }
+    where
+      nonNullString = head <$> arbitraryStringsGTZero 1 :: T.Gen String
+
+-- | Like makeFunctionSignatureList, but with existing modules.
+makeFunctionAndTypeList :: [ModuleName] -> T.Gen [FunctionSignature]
+makeFunctionAndTypeList mns = fmap (zipJoin (cycle mns) . map (\(_, b, c) -> (b, c)))
+                                makeFunctionSignatureList
+  where
+    xs `zipJoin` ys = map (\(a, (b, c)) -> (a, b, c)) (xs `zip` ys)
+
+-- | Generate function signature lists
+makeFunctionSignatureList :: T.Gen [FunctionSignature]
+makeFunctionSignatureList = do
+  fc <- T.choose (1, 50) :: T.Gen Int
+  mc <- T.choose (1, fc)
+  tc <- T.choose (1, fc)
+
+  fs <- arbitraryStringsGTZero fc
+  ms <- arbitraryStringsGTZero mc
+  ts <- arbitraryStringsGTZero tc
+
+  return (distribute ms fs ts)
+    
+arbitraryStringsGTZero
+  :: Int -> T.Gen [String]
+arbitraryStringsGTZero n = replicateM n $ T.suchThat arbitrary (\s -> not (null s) && all isAlpha s) 
+
+-- | Make a sublist, but also return all the elements of the original
+-- | list that were not in the sublist.
+sublistOfWithRemainder :: Eq a => [a] -> T.Gen ([a], [a])
+sublistOfWithRemainder xs = saveRemainder <$> T.sublistOf xs
+  where
+    saveRemainder ys = (ys, diff)
+      where
+        diff = filter (not . flip elem ys) xs
+
+-- | Assign functions to modules and types
+distribute :: [ModuleName] -> [FunctionName] -> [Type] -> [FunctionSignature]
+distribute ms fs ts = [(mn, fn, tn) | fn <- fs, mn <- ms, tn <- ts]
 
 
 insertAdded :: FunctionSignature -> Changelog -> Changelog
