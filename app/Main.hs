88e959389b2546730f17868feec76d0eeee3525a
David J. Gordon <djyale@gmail.com>
2015-10-04 20:51:53 -0400

    Changed Main to write to Html file.
    TODO: Add command line parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,19 @@
 module Main where
 
-import           Changelog
-import           System.Environment
-import FormatConsole (format)
+import Changelog
+import qualified Data.ByteString.Lazy as L
+import FormatHTML (format)
+import System.Environment
 
 main :: IO ()
 main = do
   self <- getProgName
   args <- getArgs
-  if length args /= 2
+  if length args /= 3
      then putStrLn $ ("Usage:\n" 
                            ++ self 
-                           ++ " [old hoogle db path] [new hoogle db path]\n")
-     else let [oldVersion, newVersion] = args
+                           ++ " [old hoogle db path] [new hoogle db path] [output path]\n")
+     else let [oldVersion, newVersion, outputPath] = args
           in do
               cl <- compareModules oldVersion newVersion
-              putStrLn $ format cl
-              
+              L.writeFile outputPath (format cl)
