2a778f4b6c3256e523d09b5933e90012c99d9408
David J. Gordon <djyale@gmail.com>
2015-10-10 18:16:15 -0400

    Zebra-ish tables for html output.

---
+++ src/Css.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE QuasiQuotes #-}
+module Css (writeCss) where
+import Text.Lucius
+import qualified Data.Text.Lazy.IO as TLIO
+
+-- Dummy render function
+render = undefined
+
+css :: t -> Css
+css = 
+  [lucius|
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+/* Changelog css */
+body {
+  font: normal medium/1.4 sans-serif;
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+  border: 0 solid #ccc;
+}
+
+td, th {
+  padding: 0.25rem;
+}
+
+.functionName {
+  padding: 0.25rem;
+  text-align: right;
+}
+
+.type {
+  text-align: left;
+}
+
+/* Odd and even entry needed because Changed Type entries span two lines */
+.odd-entry {
+  background: #eee;
+}
+
+.even-entry {
+
+}
+  |]
+
+writeCss :: FilePath -> IO ()
+writeCss path = TLIO.writeFile path (renderCss (css render))
--- src/FormatHTML.hs
+++ src/FormatHTML.hs
@@ -3,7 +3,7 @@ module FormatHTML (format) where
 
 import qualified Changelog as C
 import           Control.Monad (forM_, replicateM_)
-import           Data.ByteString.Lazy
+import           Data.ByteString.Lazy hiding (cycle, zip)
 import qualified Data.Map as M
 import           Text.Blaze (Markup)
 import           Text.Blaze.Html.Renderer.Utf8 (renderHtml)
@@ -22,28 +22,31 @@ format cssPath cl = renderHtml $
       H.h1 "Changelog"
       H.table $ mapM_ buildHtml (M.toList m)
 
-
+       
 
 buildHtml
   :: H.ToMarkup a => (a, C.Changelog) -> Markup
 buildHtml (m, cl) = do
   H.tr (H.th ! A.colspan (H.stringValue $ show cols) $ H.h2 (H.toHtml m))
 
-  forM_ (C.clAdded cl) $ \(_, f, t) -> 
-    H.tr $ td "+" >> td f >> td "::" >> td t 
+  forWithParityM_ (C.clAdded cl) $ \((_, f, t), parityF) ->
+    parityF $ H.tr (td "+" >> td f >> td "::" >> td t )
   blankRow
 
-  forM_ (C.clDeleted cl) $ \(_, f, t) ->
-    H.tr $ td "-" >> td f >> td "::" >> td t 
+  forWithParityM_ (C.clDeleted cl) $ \((_, f, t), parityF) ->
+    parityF $ H.tr (td "-" >> td f >> td "::" >> td t )
   blankRow
 
-  forM_ (C.clChangedType cl) $ \((_, f, t), (_, _, t')) -> do
-    H.tr $ H.td "~" >> td f >> td "::" >> td t'
-    H.tr $ emptyCell >> emptyCell >> emptyCell >> td t
+  forWithParityM_ (C.clChangedType cl) $ \(((_, f, t), (_, _, t')), parityF) -> do
+    parityF $ H.tr (H.td "~" >> td f >> td "::" >> td t')
+    parityF $ H.tr (emptyCell >> emptyCell >> emptyCell >> td t)
   blankRow
   blankRow
 
   where
+    forWithParityM_ xs = forM_ (xs `zip` cycle [evenEntry, oddEntry])
+    oddEntry = (! A.class_ "odd-entry")
+    evenEntry = (! A.class_ "even-entry")
     cols = 4
     blankRow = H.tr (replicateM_ cols emptyCell)
     emptyCell = td ""
