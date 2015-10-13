2da1001aca5765f826aff4d6e72b47bf49ae99f3
David J. Gordon <djyale@gmail.com>
2015-10-13 19:57:24 -0400

    Main can now output either as Console or Html.

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,8 @@ module Main where
 import           Changelog
 import           Css
 import qualified Data.ByteString.Lazy as L
-import           FormatHTML (format)
+import qualified FormatHTML as H
+import qualified FormatConsole as C
 import qualified System.Directory as D
 import           System.Environment
 import           System.FilePath
@@ -12,15 +13,19 @@ main :: IO ()
 main = do
   self <- getProgName
   args <- getArgs
-  if length args /= 3
+  if length args /= 4
     then putStrLn
            ("Usage:\n"
             ++ self
-               ++ " [old hoogle db path] [new hoogle db path] [output directory]\n")
-    else let [oldVersion, newVersion, outputPath] = args
+               ++ " [old hoogle db path] [new hoogle db path] [output directory] [html/console]\n")
+    else let [oldVersion, newVersion, outputPath, outputType] = args
          in do
            cl <- compareModules oldVersion newVersion
-           writeHtml outputPath cl
+           case outputType of
+             "html" -> writeHtml outputPath cl
+             "console" ->
+               putStrLn (C.format cl)
+             e -> error $ "Unsupported output type: " ++ e
 
 cssPath :: FilePath
 cssPath = "out.css"
@@ -28,7 +33,7 @@ cssPath = "out.css"
 writeHtml :: FilePath -> Changelog -> IO ()
 writeHtml outputPath cl = do 
   D.createDirectoryIfMissing True outputPath
-  L.writeFile (outputPath </> "out.html") (format cssPath cl) 
+  L.writeFile (outputPath </> "out.html") (H.format cssPath cl) 
   writeCss (outputPath </> cssPath)
 
 
